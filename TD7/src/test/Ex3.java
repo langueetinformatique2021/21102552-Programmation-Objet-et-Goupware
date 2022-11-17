945ff635ded650fed89864942f3a61de8081fbf7
ShailynnX <shailynnxie@gmail.com>
2022-11-17 10:32:15 +0100

    17/11

--- TD7/src/test/Ex3.java
+++ TD7/src/test/Ex3.java
@@ -1,10 +1,14 @@
 package test;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
 public class Ex3 {
 	public static void main(String[] args) {
 		GregorianCalendar ca = new GregorianCalendar();
+		Date date = new Date();
+		SimpleDateFormat dateFormat= new SimpleDateFormat("yyyy-MM-dd :hh:mm:ss");
+		System.out.println(dateFormat.format(date));
 		System.out.println("TIME: " + ca.getTime());
 
 
