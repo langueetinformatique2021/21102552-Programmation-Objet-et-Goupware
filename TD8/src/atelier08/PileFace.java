47d5554356c5c81a9b2f4a0e02362d2ddb0e60f3
ShailynnX <shailynnxie@gmail.com>
2022-11-30 16:25:28 +0100

    td8

--- TD8/src/atelier08/PileFace.java
+++ TD8/src/atelier08/PileFace.java
@@ -2,24 +2,19 @@ package atelier08;
 
 public class PileFace {
 	public static void main(String[] args) {
-		Aleat n = new Aleat(2);
-		
-		int i=0,j=0,pile=0,face=0;
-		double somme = 1000.0;
-		do {
-			i++;j+=guess(n);
-			if(guess(n)==0) {
-				face++;
-			}else {
-				pile++;
-			}
-		}while(i<somme);
-		System.out.println("Nombre de face:"+face);
-		System.out.println("Nombre de pile:"+pile);
+		Aleat pileface = new Aleat(2);
 
-		
+		int pile = 0, face = 0;
+		for (int i = 0; i < 1000; i++) {
+			int tire = pileface.get();
+			System.out.print(tire);
+			if (tire == 1)
+				pile++;
+			else
+				face++;
+		}
+		System.out.println();
+		System.out.println(pile + " piles et " + face + " faces");
+
 	}
-	public static int guess(Aleat nombre) {
-		return (nombre.get()>4) ? 1:0;
-				}
 }
