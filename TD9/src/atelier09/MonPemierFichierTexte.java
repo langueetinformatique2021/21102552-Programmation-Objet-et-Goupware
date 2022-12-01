669c1f583c99c2900d200c37a389d7b4412451c6
ShailynnX <shailynnxie@gmail.com>
2022-12-01 10:11:17 +0100

    ex2

--- TD9/src/atelier09/MonPemierFichierTexte.java
+++ TD9/src/atelier09/MonPemierFichierTexte.java
@@ -1,18 +1,18 @@
 package atelier09;
 
 import java.io.File;
-import java.io.InputStreamReader;
+import java.io.FileInputStream;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileInputStream;
 import java.io.FileWriter;
+import java.io.InputStreamReader;
 
 
 public class MonPemierFichierTexte {
 	static void main(String args[]) {
 	try {
 			/* ecrire un fichier txt */
-			File writename = new File(".\\result\\en\\output.txt"); 
+			File writename = new File(".\\output.txt"); 
 			writename.createNewFile(); /*creer new fichier*/
 			BufferedWriter out = new BufferedWriter(new FileWriter(writename));
 			out.write("nom prenom\r\n"); 
@@ -21,6 +21,27 @@ public class MonPemierFichierTexte {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	public static void verifier(String str) throws Exception{
+		String fileName = ".\\output.txt";
+		//loading fichier local
+		File file = new File(fileName);
+	
+        InputStreamReader read = new InputStreamReader(new FileInputStream(file),"UTF-8"); //编码格式	        
+        BufferedReader bufferedReader = new BufferedReader(read);
+		String line = null;
+		while ((line = bufferedReader.readLine()) != null) {
+			if(line.startsWith("#")){
+				continue;
+			}
+			if (line.contains(str)) {
+				System.out.println("Exist "+line);
+			}else {
+				System.out.println(str+" notExist");
+			}
+		}
+		bufferedReader.close();
+
 	}
 }
 
