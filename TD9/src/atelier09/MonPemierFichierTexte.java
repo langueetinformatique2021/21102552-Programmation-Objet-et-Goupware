90c1c61a0d4168edf254482b8f0fd17a938ff5b6
ShailynnX <shailynnxie@gmail.com>
2022-12-01 09:54:56 +0100

    td9

--- TD9/src/atelier09/MonPemierFichierTexte.java
+++ TD9/src/atelier09/MonPemierFichierTexte.java
@@ -1,5 +1,26 @@
 package atelier09;
 
-public class MonPemierFichierTexte {
+import java.io.File;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileWriter;
 
+
+public class MonPemierFichierTexte {
+	static void main(String args[]) {
+	try {
+			/* ecrire un fichier txt */
+			File writename = new File(".\\result\\en\\output.txt"); 
+			writename.createNewFile(); /*creer new fichier*/
+			BufferedWriter out = new BufferedWriter(new FileWriter(writename));
+			out.write("nom prenom\r\n"); 
+			out.flush(); 
+			out.close(); 
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
+
