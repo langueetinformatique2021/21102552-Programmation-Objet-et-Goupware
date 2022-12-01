42b76ff59aa0612f5fc0acc91adab271b6263d64
ShailynnX <shailynnxie@gmail.com>
2022-12-01 10:19:53 +0100

    copy

---
+++ TD9/src/atelier09/copy.java
@@ -0,0 +1,27 @@
+package atelier09;
+
+import java.io.*;
+
+public class copy{
+	@SuppressWarnings("unused")
+	private static void copyFileUsingFileStreams(File source, File dest)
+	        throws IOException {    
+	    InputStream input = null;    
+	    OutputStream output = null;    
+	    try {
+	           input = new FileInputStream(source);
+	           output = new FileOutputStream(dest);        
+	           byte[] buf = new byte[1024];        
+	           int bytesRead;        
+	           while ((bytesRead = input.read(buf)) > 0) {
+	               output.write(buf, 0, bytesRead);
+	           }
+	    } finally {
+	        input.close();
+	        output.close();
+	    }
+	}
+
+
+
+}
